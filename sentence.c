d659bc7fd2b3a1ed3759ca00ffbc2e6d639e5845
Nathan <nathanroane@gmail.com>
2021-04-06 23:25:52 -0400

    Update sentence.c

--- sentence.c
+++ sentence.c
@@ -10,10 +10,17 @@
  * 
  * 
  * Todo: Put buffer checks on every input
- * 			-Need to figure out appropriate buffer size for the typist functions in general
+ *			-Need to figure out appropriate buffer size for the typist functions in general
  * 
- * 		 Try and make it overall portable or at least very multi-terminal friendly
+ * 		Try and make it overall portable or at least very multi-terminal friendly
  * 
+ * 		Enable the boolean parameter for toggling the typo feature
+ * 			-Re-factor the code to fit into a typos/no typos algorithm
+ * 
+ *		KNOWN BUGS:
+ * 			-The typo function clashes with the terminal colour commands
+ * 				*Probably need to increase the safe-zone for typos at the beginning and end of strings by a bit to fix this.s
+ * 				** FIXED? April 6 2021
  */
 
 
@@ -32,21 +39,62 @@ void typist(char sentence[150]){
 	int sentence_length;
 	char nextLetter;
 	
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char typoLetter;
 	sentence_length = strlen(sentence);
+	int backspaces;				// This is the number of times the delete button will be hit to go back and fix a typo, makes it feel natural
+	int mistakePercent;			// I want this to be an int, 0 - 99, if it is <5 (so a 5% chance) then a typo will be made
+	int whichLetter;			// which letter the typo will be
 	
 	for (int i=0; i < sentence_length; i++) {
 		
-		if (sentence[i] == ',' || sentence[i] == '.'){
+		mistakePercent = rand() % 99;	// random 0 - 99, see mistakePercent above
+
+		if ("%c", sentence[i] == ' '){
 			printf("%c", sentence[i]);
-			//printf("%c", sentence[i+1]);
-			fflush(stdout);
+			continue;
+		}
+		
+		if ("%c", sentence[i] == '.' || "%c", sentence[i] == ','){
+			printf("%c", sentence[i]);
 			usleep(1075000);
+			continue;
+		}
+		
+		if (mistakePercent < 5 && i > 8 && i < sentence_length-9) {
+			
+			whichLetter = rand() % 25;
+		
+			backspaces = (rand() % 70)/10;
+			printf("%c", alphabet[whichLetter]);		//print the letter the typo will be
+			//printf("\nMISTAKE\n");
+			//printf("%i", i);
+			//printf("\nMISTAKE\n");
+			
 			fflush(stdout);
-		} else {	
-			printf("%c", sentence[i]);
 			usleep(75000);
 			fflush(stdout);
+			i++;		// increment i so the next letter is still accurate
+			
+			for (int x=0; x < backspaces; x++){		// This is the very next letter for backspaces number of letters
+				printf("%c", sentence[i]);
+				usleep(75000);
+				fflush(stdout);
+				i++;
+			}
+				
+			for (int y=backspaces+1; y > 0; y--){
+				printf("\b \b");
+				usleep(100000);
+				fflush(stdout);
+				i--;
+			}
+			
 		}
+		
+		printf("%c", sentence[i]);
+		usleep(75000);
+		fflush(stdout);
 	}
 
 	usleep(1175000);
@@ -59,25 +107,7 @@ void typist_fast(char sentence[150]){
 	
 	int sentence_length;
 	char nextLetter;
-	
-	sentence_length = strlen(sentence);
-	
-	for (int i=0; i < sentence_length; i++) {
-		
-		printf("%c", sentence[i]);
-		usleep(75000);
-		fflush(stdout);
-	}
-	
-	usleep(1175000);
-	printf("\n");
-}
 
-
-void typist_urgent(char sentence[150], bool typos, bool lineBreak){
-	
-	int sentence_length;
-	char nextLetter;
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	char typoLetter;
 	sentence_length = strlen(sentence);
@@ -102,7 +132,78 @@ void typist_urgent(char sentence[150], bool typos, bool lineBreak){
 			continue;
 		}
 		
-		if (mistakePercent < 5 && i > 3 && i < sentence_length-5) {
+		if (mistakePercent < 5 && i > 8 && i < sentence_length-9) {
+			
+			whichLetter = rand() % 25;
+		
+			backspaces = (rand() % 70)/10;
+			printf("%c", alphabet[whichLetter]);		//print the letter the typo will be
+			//printf("\nMISTAKE\n");
+			//printf("%i", i);
+			//printf("\nMISTAKE\n");
+			
+			fflush(stdout);
+			usleep(75000);
+			fflush(stdout);
+			i++;		// increment i so the next letter is still accurate
+			
+			for (int x=0; x < backspaces; x++){		// This is the very next letter for backspaces number of letters
+				printf("%c", sentence[i]);
+				usleep(75000);
+				fflush(stdout);
+				i++;
+			}
+				
+			for (int y=backspaces+1; y > 0; y--){
+				printf("\b \b");
+				usleep(125000);
+				fflush(stdout);
+				i--;
+			}
+			
+		}
+		
+		printf("%c", sentence[i]);
+		usleep(25000);
+		fflush(stdout);
+	}
+	
+	usleep(1175000);
+	printf("\n");
+}
+
+
+void typist_urgent(char sentence[150], bool typos, bool lineBreak){
+	
+	int sentence_length;
+	char nextLetter;
+	
+	
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char typoLetter;
+	sentence_length = strlen(sentence);
+	int backspaces;				// This is the number of times the delete button will be hit to go back and fix a typo, makes it feel natural
+	int mistakePercent;			// I want this to be an int, 0 - 99, if it is <5 (so a 5% chance) then a typo will be made
+	int whichLetter;			// which letter the typo will be
+	
+	for (int i=0; i < sentence_length; i++) {
+		
+		mistakePercent = rand() % 99;	// random 0 - 99, see mistakePercent above
+		
+		
+		//printf("%i", mistakePercent);
+		//printf("\n");
+		//printf("%i", backspaces);
+		//printf("\n");
+		//printf("%i", whichLetter);
+		//printf("\n");
+		
+		if ("%c", sentence[i] == ' '){
+			printf("%c", sentence[i]);
+			continue;
+		}
+		
+		if (mistakePercent < 5 && i > 8 && i < sentence_length-9) {
 			
 			whichLetter = rand() % 25;
 		
@@ -170,13 +271,13 @@ void menu(){
 	fflush(stdout);
 	int a;
 	fflush(stdout);
-    typist_urgent("What would you like to do?:\n", true, true);
+    typist_urgent("What would you like to do?:\n", false, true);
     fflush(stdout);
     typist_urgent("1) Go down the Rabbit Hole\n", false, true);
     fflush(stdout);
     typist_urgent("2) Restart this application\n", false, true);
     fflush(stdout);
-    typist_urgent("3) Quit", true, true);
+    typist_urgent("3) Quit", false, true);
     fflush(stdout);
     typist_urgent("\nThe choice is yours: ", false, true);
     fflush(stdout);
